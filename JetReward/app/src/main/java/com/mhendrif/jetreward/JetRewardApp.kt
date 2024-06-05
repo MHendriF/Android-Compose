9b64d0e176f5c2d136bd9d391d69398e7d5ca696
MHendriF <hendrifebriansyah28@gmail.com>
2024-06-05 14:25:56 +0700

    implement popBackStack

--- JetReward/app/src/main/java/com/mhendrif/jetreward/JetRewardApp.kt
+++ JetReward/app/src/main/java/com/mhendrif/jetreward/JetRewardApp.kt
@@ -129,6 +129,14 @@ fun JetRewardApp(
                         navController.navigateUp()
                     },
                     navigateToCart = {
+                        navController.popBackStack()
+                        navController.navigate(Screen.Cart.route) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
                     }
                 )
             }
