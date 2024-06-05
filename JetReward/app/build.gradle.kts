b9f6904735ea331760c368182f23fd782a5f8307
MHendriF <hendrifebriansyah28@gmail.com>
2024-06-05 14:13:01 +0700

    implement bottom navigation

--- JetReward/app/build.gradle.kts
+++ JetReward/app/build.gradle.kts
@@ -67,4 +67,5 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation(libs.lifecycle.viewmodel.compose)
+    implementation(libs.navigation.compose)
 }
